483563bce61076821ac89b0cfa9ee41651fa7203
Jan Eglinger <jan.eglinger@fmi.ch>
2026-06-23 14:33:00 +0200

    Initialize fractal task repository

---
+++ src/faim_fractal_converters/dev/task_list.py
@@ -0,0 +1,24 @@
+"""Contains the list of tasks available to fractal."""
+
+from fractal_task_tools.task_models import (
+    ConverterCompoundTask,
+)
+
+AUTHORS = "Facility for Advanced Imaging and Microscopy (FAIM), FMI, Basel"
+
+DOCS_LINK = None
+
+
+TASK_LIST = [
+    ConverterCompoundTask(
+        name="Convert Visiview companion.ome",
+        executable_init="convert_visiview_init_task.py",
+        executable="single_image_compute_task.py",
+        meta_init={},
+        meta={},
+        category="Conversion",
+        tags=[
+            "Visiview",
+        ]
+    )
+]
